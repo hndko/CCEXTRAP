47cb7c856161d1ea7fa23397ae3b96705ede9875
Handoko <kyuoko13@gmail.com>
2022-10-22 15:28:19 +0700

    merubah file cc.class.php

--- cc.class.php
+++ cc.class.php
@@ -111,7 +111,7 @@ class CC
     protected function Check($card)
     {
         $headers = array();
-        $headers[] = 'origin: https://amznloot.com';
+        $headers[] = 'origin: ';
         $headers[] = 'accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7';
         $headers[] = 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36';
         $headers[] = 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8';
